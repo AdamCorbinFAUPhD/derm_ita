8a92651b7d9fd8d000edf88597534690923efe70
Adam Corbin <acorbin3@gmail.com>
2021-12-11 04:28:39 -0500

    Adding different modified fitzpatrick_scales

--- derm_ita/fitzpatrick_scale.py
+++ derm_ita/fitzpatrick_scale.py
@@ -1,6 +1,34 @@
-def get_fitzpatrick_type(ita):
+def get_kinyanjui_type(ita):
     """
     This function will take a ita value and return the Fitzpatrick skin tone scale
+    https://arxiv.org/pdf/1910.13268.pdf
+    :param ita:
+    :return:
+    """
+    if ita <= 10:
+        return "dark"
+    elif 10 < ita <= 19:
+        return "tan1"
+    elif 19 < ita <= 28:
+        return "tan2"
+    elif 28 < ita <= 34.5:
+        return "int1"
+    elif 34.5 < ita <= 41:
+        return "int2"
+    elif 41 < ita <= 48:
+        return "lt1"
+    elif 48 < ita <= 55:
+        return "lt2"
+    elif 55 < ita:
+        return "very_lt"
+    else:
+        print(f"None cat: {ita}")
+
+
+def get_kinyanjui_groh_type(ita):
+    """
+    This function will take a ita value and return the Fitzpatrick skin tone scale
+    https://openaccess.thecvf.com/content/CVPR2021W/ISIC/papers/Groh_Evaluating_Deep_Neural_Networks_Trained_on_Clinical_Images_in_Dermatology_CVPRW_2021_paper.pdf
     :param ita:
     :return:
     """
@@ -20,7 +48,30 @@ def get_fitzpatrick_type(ita):
         print(f"None cat: {ita}")
 
 
-def get_fitzpatrick2_type(ita):
+def get_del_bino_type(ita):
+    """
+    This function will take a ita value and return the Fitzpatrick skin tone scale
+    https://journals.plos.org/plosone/article/file?id=10.1371/journal.pone.0241843&type=printable
+    :param ita:
+    :return:
+    """
+    if ita < -30:
+        return "dark"
+    elif -30 < ita <= 10:
+        return "brown"
+    elif 10 < ita <= 28:
+        return "tan"
+    elif 28 < ita <= 41:
+        return "intermediate"
+    elif 41 < ita <= 55:
+        return "light"
+    elif 55 < ita:
+        return "very light"
+    else:
+        print(f"None cat: {ita}")
+
+
+def get_fitzpatrick_type(ita):
     """
     This function will take a ita value and return the Fitzpatrick skin tone scale
     https://arxiv.org/pdf/2104.14685.pdf
@@ -44,15 +95,21 @@ def get_fitzpatrick2_type(ita):
 
 
 def add_groh_ita_category(ita):
+    """
+    Developed based on the empirical distribution of ITA scores minimizing overall error
+    https://openaccess.thecvf.com/content/CVPR2021W/ISIC/papers/Groh_Evaluating_Deep_Neural_Networks_Trained_on_Clinical_Images_in_Dermatology_CVPRW_2021_paper.pdf
+    :param ita:
+    :return:
+    """
     if ita <= -25:
-        return "Type6"
+        return "6"
     elif -25 < ita <= 0:
-        return "Type5"
+        return "5"
     elif 0 < ita <= 12:
-        return "Type4"
+        return "4"
     elif 12 < ita <= 23:
-        return "Type3"
+        return "3"
     elif 23 < ita <= 40:
-        return "Type2"
+        return "2"
     elif 40 < ita:
-        return "Type1"
+        return "1"
